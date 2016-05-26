7157daf65273f2a10cf4e710a8f1d3544cbc238c
Simon Cahill <Beatsleigher@gmail.com>
2016-05-26 20:33:00 +0200

    did it

---
+++ MvvmBase/MvvmBase_CS/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CaSoftworks.MvvmBase {
+
+    using System.ComponentModel;
+
+    public class ViewModel: INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Invokes the PropertyChanged event for this class.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that has changed.</param>
+        protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+    }
+}
---
+++ MvvmBase/MvvmBase_VB/ViewModel.vb
@@ -0,0 +1,20 @@
+﻿Option Strict On
+
+Imports System.ComponentModel
+
+Namespace CaSoftworks.MvvmBase
+
+
+    Public MustInherit Class ViewModel
+
+        Implements INotifyPropertyChanged
+
+        Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+        Protected Sub OnPropertyChanged(propertyName As String)
+
+        End Sub
+
+    End Class
+
+End Namespace
